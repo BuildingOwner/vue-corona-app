1b15ea8e55ad0f4d3ee78a77249602cc433cbc35
BuildingOwner <106369213+BuildingOwner@users.noreply.github.com>
2023-02-27 22:17:11 +0900

    dev layout

---
+++ src/components/corona/DomesticCases.vue
@@ -0,0 +1,11 @@
+<template>
+  <h2>Domestic</h2>
+</template>
+
+<script>
+export default {
+  name: "domasticCases",
+};
+</script>
+
+<style></style>
---
+++ src/components/corona/InternationalCases.vue
@@ -0,0 +1,11 @@
+<template>
+  <h2>International</h2>
+</template>
+
+<script>
+export default {
+  name: "internationalCases",
+};
+</script>
+
+<style></style>
